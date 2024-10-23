6af2b0c036810aeb759a31859e1235548cf5e2ae
Kirill <kvn2001@internet.ru>
2024-10-23 22:55:18 +0300

    two-excel

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,64 +1,71 @@
-
-
-const headTab = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const headTab = [ "","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 
 document.addEventListener("DOMContentLoaded", () => {
-    const spreadsheet = document.getElementById("table");
-    const rows = 13; 
-    const cols = 14; 
+  const spreadsheet = document.getElementById("table");
+  const rows = 13;
+  const cols = headTab.length;
 
-    const head = spreadsheet.createTHead(); 
-    const headerRow = head.insertRow(); 
+  const head = spreadsheet.createTHead();
+  const headerRow = head.insertRow();
+
+  headTab.map((head) => {
+    const th = document.createElement("th");
+    th.textContent = head;
+    head[0] ? th.classList.add("thead_th") : th.classList.add("thead_start");
+    headerRow.appendChild(th);
+    return th;
+  });
+
+  const body = spreadsheet.createTBody();
+  for (let i = 0; i < rows; i++) {
+    const row = body.insertRow();
+
     for (let j = 0; j < cols; j++) {
-        const th = document.createElement("th"); 
-        th.textContent = headTab[j];
-        th.classList.add("thead_th");
-        headerRow.appendChild(th); 
-    }
+      let count = 0;
+      const cell = row.insertCell();
+      cell.contentEditable = true;
+      cell.draggable = true;
 
-    const body = spreadsheet.createTBody(); 
-    for (let i = 0; i < rows; i++) {
-        const row = body.insertRow();
-        
-        for (let j = 0; j < cols; j++) {
-            const cell = row.insertCell(); 
-            cell.contentEditable = true;  
-            cell.draggable = true;
+      if (j === 0) {
+        cell.textContent = i + 1;
+        cell.contentEditable = false;
+        cell.classList.add("tbody_start");
+      } else {
+        cell.textContent = "";
+      }
 
-            cell.addEventListener("dragstart", (event) => {
-                event.dataTransfer.setData('text/plain', event.target.textContent);
-                event.target.classList.add('selected');
-            });
+      cell.addEventListener("dragstart", (event) => {
+        event.dataTransfer.setData("text/plain", event.target.textContent);
+        event.target.classList.add("selected");
+      });
 
-            cell.addEventListener("dragend", (event) => {
-                event.target.classList.remove('selected');
-            });
+      cell.addEventListener("dragend", (event) => {
+        event.target.classList.remove("selected");
+      });
 
-            cell.addEventListener("dragover", (event) => {
-                event.preventDefault(); // Prevent default to allow drop
-                event.target.classList.add('dropzone');
-            });
+      cell.addEventListener("dragover", (event) => {
+        event.preventDefault(); // Prevent default to allow drop
+        event.target.classList.add("dropzone");
+      });
 
-            cell.addEventListener("dragleave", (event) => {
-                event.target.classList.remove('dropzone');
-            });
+      cell.addEventListener("dragleave", (event) => {
+        event.target.classList.remove("dropzone");
+      });
 
-            cell.addEventListener("drop", (event) => {
-                event.preventDefault();
-                const data = event.dataTransfer.getData('text/plain');
-                const droppedCell = event.target;
-                if (droppedCell !== null) {
-                    droppedCell.textContent = data; // Set data to the target cell
-                }
-                event.target.classList.remove('dropzone');
-            });
-
-
-            cell.addEventListener('dblclick', () => {
-                cell.textContent = ''; // Clear the cell content
-            });
-
-
+      cell.addEventListener("drop", (event) => {
+        event.preventDefault();
+        const data = event.dataTransfer.getData("text/plain");
+        const droppedCell = event.target;
+        if (droppedCell !== null) {
+          droppedCell.textContent = data; // Set data to the target cell
         }
+        event.target.classList.remove("dropzone");
+      });
+
+      cell.addEventListener("dblclick", () => {
+        if (j === 0) return;
+        cell.textContent = "";
+      });
     }
-})
+  }
+});
